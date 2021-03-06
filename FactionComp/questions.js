11a68e2750c7eef54f3b896bd70365d9be47995f
RandomestBluer <79170455+RandomestBluer@users.noreply.github.com>
2021-03-06 21:59:39 +0100

    Update questions.js

--- FactionComp/questions.js
+++ FactionComp/questions.js
@@ -1,4 +1,4 @@
 questions =  [
-    {"id"=0, "question": Strongly Agree, "effects": {"war":1}},
-    {"id"=1, "question": Strongly Disagree, "effects": {"auth":1}}
+    {"id"=0, "question": "Strongly Agree", "effects": {"war":1}},
+    {"id"=1, "question": "Strongly Disagree", "effects": {"auth":1}}
 ];
